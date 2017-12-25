31d48cb08007d712f35fc0601ac4a67d084e4bde
Ammar Faizi <ammarfaizi2@gmail.com>
2017-12-25 15:21:07 +0700

    Fix

--- a.php
+++ a.php
@@ -1,28 +1,64 @@
 <?php
 
-ini_set("disable_functions", implode(",", get_defined_functions()['internal']));
+$exceptions = ["get_defined_functions", "file_get_contents", "show_source", "strlen", "rtrim", "implode", "define", "rand"];
 
-// define QQQQ
-// define RRRR
+$forbidden = "";
+
+foreach(get_defined_functions()['internal'] as $q) {
+	if (! in_array($q, $exceptions)) {
+		$forbidden .= $q.",";
+	}
+}
+
+$forbidden = rtrim($forbidden, ",");
+ini_set("display_errors", true);
+ini_set("disable_functions", $forbidden);
+
+
+// define QQQQ and define RRRR from secret.php
 include "secret.php";
 
 if (isset($_FILES['qqqq'], $_POST['class_name'])) {
-	$q = file_get_contents($_FILES['file']['tmp_name']);
-	if (strlen($q) > 100) {
-		exit("Error");
+
+	$q = file_get_contents($_FILES['qqqq']['tmp_name']);
+
+	if ((strpos($q, "require")!== false) || (strpos($q, "include") !== false) || (strpos($q, "^") !== false) || (strpos($q, "\\x5e")!==false)) {
+		
+		exit("Error"); 
+
 	} else {
 
-		spl_autoload_register(function ($class)  {
-			require $_FILES['file']['tmp_name'];
-		});
+		require $_FILES['qqqq']['tmp_name']; // Load uploaded file.
 
+		$salty = 61529519452809720693702583126814;
+
+		if ((strcmp($salty, $x) !== 0 && $x !== $salty) || strlen($q) > 1000) {
+			exit("Error 2");
+		}
+	
 
 		$q = new $_POST['class_name'];
-		if ($q->q === 2 && $q->q === 3 && $q->q === 4 && $q->q === 5 && $q->q === 10) {
+
+		if ($q->q === 2 && $q->q === 3 && $q->q === 4 && $q->q === 10 && $q->q === "Hello World!" && $q->q === "https://t.me/TeaInside") {
 			if ($q['qqqq'] === QQQQ && $q['rrrr'] === (RRRR - 1)) {
-				
+				if ($q."eaInside" === "@TeaInside") {
+					
+					$randFunc = "BlablaFunction".rand(0, 10000).time();
+					$randFunc2 = "BlablaStaticFunction".rand(0, 10000).time();
+
+					if ($q->{$randFunc}() === "A" && ($_POST['class_name'])::{$randFunc2}() === "B") {
+						include "^"; // success
+						exit();
+					}
+				}
+
+				exit("Error 5");
 			}
+
+			exit("Error 4");
 		}
+
+		exit("Error 3");
 	}
 } else {
 	show_source(__FILE__);
--- index.php
+++ index.php
@@ -1,3 +1,13 @@
-<?php
-
-echo implode(",", get_defined_functions()['internal']);
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+<form action="a.php" method="post" enctype="multipart/form-data">
+	<input type="file" name="qqqq">
+	<input type="text" name="class_name">
+	<input type="submit" name="">
+</form>
+</body>
+</html>
